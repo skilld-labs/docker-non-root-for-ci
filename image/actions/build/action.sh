4f205c317204220260f9a23fe94a565f5e845f73
David Ferlay <dferlay@skilld.cloud>
2025-03-18 17:29:10 +0100

    Adding actions to build and push images

--- image/actions/build/action.sh
+++ image/actions/build/action.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
+date
+pwd
 whoami
-id
-docker build --help
-service docker status || true
-service containerd status || true
-service docker start
-service containerd start || true
-service docker status || true
-service containerd status || true
-docker --version
-export VERSION=111.111
-docker build -t skilldlabs/docker-non-root-for-ci:${VERSION}-r --build-arg ALPINE_VERSION=${VERSION} --build-arg GID=233 --build-arg GNAME=docker --build-arg UNAME=skilld .
+env
+
